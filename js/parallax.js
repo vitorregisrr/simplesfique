ba1e529dc5a10759c767383cd525d9b7fe276c82
vitorregisr <vitorregisrr@gmail.com>
2019-07-02 13:40:42 -0300

    Add offsetY-md feature to parallax.js and add to some shapes inside HTML

--- js/parallax.js
+++ js/parallax.js
@@ -2,6 +2,13 @@ $(window).scroll(function () {
     $('.shape-parallax').each(function(key, elem) {
         var scrollSpeed = $(elem).attr('data-parallax') || 2;
         var offsetY =  parseInt( $(elem).attr('data-parallax-offsetY'))|| 0;
+        var offsetY_md =  parseInt( $(elem).attr('data-parallax-offsetY-md'))|| 0;
+
+        if(window.innerWidth < 768){
+            if( offsetY_md ){
+                offsetY = offsetY_md;
+            }
+        }
 
         $(elem).css({
             'top': -($(window).scrollTop()/ scrollSpeed) + offsetY + "px"
