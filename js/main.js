5a68e33fca039b94d902cc3da4c24920859d67a6
vitorregisr <vitorregisrr@gmail.com>
2019-07-02 21:08:14 -0300

    Add countup animation, Add wrapper to differentials (fix wow animation bugs), Improve modal style, Misc improves

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@
     //scroll top
     var scrollTopBtn = $('.scroll-top');
 
+    //Util functions
+    function checkVisible(elm) {
+        var rect = elm.getBoundingClientRect();
+        var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+        return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+      }
+
     $(window).scroll(function () {
         if ($(window).scrollTop() > 300) {
             scrollTopBtn.addClass('show');
@@ -22,28 +29,33 @@
         }, '300');
     });
 
-    $('.count').appear(function () {
-        console.log('a')
-        var $this = $(this);
-        if ($this.attr('data-counted') == 'false') {
-            $this.attr('data-counted', true);
-
-            setTimeout(function () {
-                jQuery({
-                    Counter: 0
-                }).animate({
-                    Counter: $this.text()
-                }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.ceil(this.Counter));
-                    }
-                });
-            }, 200);
-        }
-    });
-
+    //Count up script
+    $(window).scroll(function () {
+        $('.count').each(function () {
+            if (checkVisible(this)) {
+                
+                const $this = $(this);
+                if ($this.attr('data-counted') == 'false') {
+                    $this.attr('data-counted', true);
+        
+                    setTimeout(function () {
+                        jQuery({
+                            Counter: 0
+                        }).animate({
+                            Counter: $this.text()
+                        }, {
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function () {
+                                $this.text(Math.ceil(this.Counter));
+                            }
+                        });
+                    }, 200);
+                }
+            }
+        })
+    })
+    
     window.dispatchEvent(new Event('resize'));
 
 })();
